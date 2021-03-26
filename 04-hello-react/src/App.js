04d2ba295b017b9afbd20400e15f764acd6c527b
seuha516 <seuha516@naver.com>
2021-03-26 21:09:17 +0900

    4~5

--- 04-hello-react/src/App.js
+++ 04-hello-react/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import EventPractice from "./components/EventPractice";
+import EventPractice2 from "./components/EventPractice2";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <EventPractice />
+      <hr />
+      <EventPractice2 />
+    </>
   );
-}
+};
 
 export default App;
