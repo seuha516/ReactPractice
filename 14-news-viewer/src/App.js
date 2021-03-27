71067f14ff4cde72a3dd04a34f60f57d2d280ce0
seuha516 <seuha516@naver.com>
2021-03-27 19:39:41 +0900

    14

---
+++ 14-news-viewer/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router";
+import NewsPage from "./pages/NewsPage";
+
+const App = () => {
+  return (
+    <>
+      <Route path="/:category?" component={NewsPage} />
+    </>
+  );
+};
+
+export default App;
---
+++ 14-news-viewer/src/components/NewsList.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+import axios from "axios";
+import usePromise from "../lib/usePromise";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+const NewsList = ({ category }) => {
+  // const [articles, setArticles] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const query = category === "all" ? "" : `&category=${category}`;
+  //       const response = await axios.get(
+  //         `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=4a8f8b16fe7449379a341702407be9c3`
+  //       );
+  //       setArticles(response.data.articles);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //     setLoading(false);
+  //   };
+  //   fetchData();
+  // }, [category]);
+  const [loading, response, error] = usePromise(() => {
+    const query = category === "all" ? "" : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`
+    );
+  }, [category]);
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+  if (!response) {
+    return null;
+  }
+  if (error) {
+    return <NewsListBlock>Error!</NewsListBlock>;
+  }
+  const { articles } = response.data;
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
